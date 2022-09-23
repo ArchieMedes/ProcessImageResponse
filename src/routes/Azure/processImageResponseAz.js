d977ed4e26ff9078684fcc042a92db6acd3ee48d
DanielZanabria <dzanabria@intevolution.ai>
2022-09-23 18:29:55 -0500

    added an alternate version for azure app fucntions

---
+++ src/routes/Azure/processImageResponseAz.js
@@ -0,0 +1,44 @@
+module.exports = async function (context, req) {
+    
+    console.log(`Consumo de API ProcessImageResponse de tipo POST`);
+    const { Option } = req.body;
+    
+    if( Option === '1' ){
+        
+        /*let responseBody = {
+            response: {
+                code: "00",
+                webServiceResponse: 'https://stgaccintebotne5t7ecgyzx.blob.core.windows.net/multimedia/tal vez.png' 
+            }
+        }
+
+        res.send(responseBody);*/
+        
+        context.res = {
+            body: {
+                response: {
+                    code: "00",
+                    webServiceResponse: 'https://stgaccintebotne5t7ecgyzx.blob.core.windows.net/multimedia/tal vez.png' 
+                }
+            }  
+        };
+    } else {
+        
+        /*let responseBody = {
+            response: {
+                code: "01",
+                webServiceResponse: 'ERROR A LA HORA DE RECIBIR LA PETICIÓN. ERROR EN LA PETICIÓN' 
+            }
+        }
+        res.send(responseBody);*/
+        
+        context.res = {
+            body: {
+                response: {
+                    code: "01",
+                    webServiceResponse: 'ERROR A LA HORA DE RECIBIR LA PETICIÓN. ERROR EN LA PETICIÓN' 
+                }
+            }  
+        };
+    }
+}
